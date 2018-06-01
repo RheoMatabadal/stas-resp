35fb1d694542e77b3d3f697007194e775a99a1a2
rheo <rheo.matabadal.natin@gmail.com>
2018-06-01 15:08:34 -0300

    no charts just yet

--- js/custom.js
+++ js/custom.js
@@ -8,24 +8,4 @@ $(document).ready(function() {
 		slideWidth: 0,
 		shrinkItems:true
 	});
-// 	var windowWidth = $(window).width(),
-// 		windowHeight = $(window).height(),
-// 		adjHeight = windowHeight;
-// 	$('.classy').css({
-// 		'width': windowWidth ,
-// 		'height': adjHeight 
-// 	});
-// });
-// $(window).resize(function() {
-// 	var windowWidth = $(window).width(),
-// 		windowHeight = $(window).height(),
-// 		adjHeight = windowHeight;
-// 	$('.classy').css({
-// 		'width': windowWidth,
-// 		'height': adjHeight
-// 	});
-	// $('.classy video').css({
-	// 	'min-width': windowWidth + 'px',
-	// 	'min-height': adjHeight + 'px'
-	// });
  });
