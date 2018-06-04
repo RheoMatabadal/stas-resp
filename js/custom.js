113a007c688f0927526905248ec0ab5296338197
rheo <rheo.matabadal.natin@gmail.com>
2018-06-04 11:13:23 -0300

    werkende load more toegevoegd

--- js/custom.js
+++ js/custom.js
@@ -9,3 +9,17 @@ $(document).ready(function() {
 		shrinkItems:true
 	});
  });
+
+$( document ).ready(function () {
+		$(".gallery").slice(0, 4).show();
+		if ($(".gallery:hidden").length != 0) {
+			$("#loadMore").show();
+		}		
+		$("#loadMore").on('click', function (e) {
+			e.preventDefault();
+			$(".gallery:hidden").slice(0, 8).slideDown();
+			if ($(".gallery:hidden").length == 0) {
+				$("#loadMore").fadeOut('slow');
+			}
+		});
+	});
