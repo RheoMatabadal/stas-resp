d4855fa9cd4101a1c3fef91fdb3bb35d61626683
rheo <rheo.matabadal.natin@gmail.com>
2018-06-07 13:36:30 -0300

    filter werkt, behalve all

--- js/custom.js
+++ js/custom.js
@@ -48,3 +48,25 @@ $(document).ready(function() {
     interval: 6000,
   });
     });
+
+//portfolio filter
+$(document).ready(function(){
+    $(".button").click(function(){
+      var value = $(this).attr("data-filter");
+      if (value == "all") {
+        $(".filter").show("1000");
+
+      }
+      else
+      {
+        $(".filter").not("."+value).hide("1000");
+        $(".filter").filter("."+value).show("1000");
+
+      }
+      //add active class
+      $("ul .button").click(function(){
+      	$(this).addClass('active').siblings().removeClass('active');
+      });
+      
+    });
+  });
